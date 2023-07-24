e248fcdb7ba2eab4c2de7cb9eb96d429a04ba432
KarinRG <131704279+KarinRG@users.noreply.github.com>
2023-07-24 22:38:52 +0000

    proyecto

---
+++ test.js
@@ -0,0 +1,8 @@
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(1)).toBe(1.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
